f3b74beac5cd0d7af6b9bc2a1eb8299ad5b987c6
RadHazzard <radhazzard@gmail.com>
2018-08-23 15:32:09 -0600

    Added file splitting, resource gathering

--- passcheck.py
+++ passcheck.py
@@ -1,4 +1,26 @@
+import pip
 from hashlib import sha1
+from multiprocessing import cpu_count
+
+try:
+    import psutil
+except ImportError:
+    print("[ ! ] Package 'psutil' not found. This must be installed.")
+    print("[ ! ] Try running 'python -m pip install psutil' as administrator and try again.")
+    return
+try:
+    import filesplit
+except ImportError:
+    print("[ ! ] Package 'filesplit' not found. This must be installed.")
+    print("[ ! ] Try running 'python -m pip install filesplit' as administrator and try again.")
+    return
+
+print("[ + ] Setting number of threads based on logical cores...")
+threadcount = cpu_count()
+print("[ + ] Set to", threadcount, "threads.")
+print("[ + ] Determining split file size based on available memory...")
+mem = psutil.virtual_memory()
+filesize = mem.available/2 # Only going to try and use 1/2 of available mem for any given action
 inpath = "" # Password file location
 x = sha1(input("Password to look for: ").encode("utf-8"))
 p = 0
