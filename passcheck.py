9b7c82832f872a364f440f406be4ced6e5664007
RadHazzard <radhazzard@gmail.com>
2019-09-25 19:18:52 -0600

    Update code behavior

--- passcheck.py
+++ passcheck.py
@@ -1,18 +1,45 @@
-from hashlib import sha1
-# Possibly use filesplit from pypi to split files to more manageable sizes.
-# Split files down and store elsewhere, then prep each file for multithreading. 
-inpath = "" # Password file location
-x = sha1(input("Password to look for: ").encode("utf-8"))
-p = 0
-pp = 0
-with open(inpath) as infile:
-    for line in infile:
-        p += 1
-        if x.hexdigest().upper() in line:
-            print("Hash found: \n")
-            print(line)
+import getpass
+import timeit
+import hashlib
+
+file_location = input(f"Location of file (ex: C:\\filename.txt): ")
+found = False
+with open(file_location) as file:
+    beginning = 0
+    firstline = file.readline()
+    if len(firstline.split(":")[0]) > 32:
+        hashlength = 40
+        print("SHA-1 File detected.")
+        inquery = hashlib.sha1(getpass.getpass(f"Password to look for: ").encode("utf-8")).hexdigest().upper()
+    else:
+        hashlength = 32
+        print("NTLM File detected.")
+        inquery = hashlib.new('md4',getpass.getpass(f"Password to look for: ").encode("utf-16le")).hexdigest().upper()
+    if inquery in firstline: # Check the first line before starting.
+        ending = 0
+    else:
+        ending = file.seek(0, 2)
+    start_time = timeit.default_timer()
+    while ( ending - beginning > hashlength+4 ):
+        bookmark = file.seek((ending + beginning)/2, 0)
+        current_line = file.readline()
+        while len(current_line.split(":")[0]) < hashlength: # Check if the first part of the split is too short (partial hash, or none at all)
+            bookmark = bookmark - (hashlength - len(current_line.split(":")[0])) # Determine number of characters missing, and adjust bookmark as necessary
+            file.seek(bookmark)
+            current_line = file.readline() # Re-assign the string, repeat process until aligned.
+        file.seek(bookmark) # Return to previous line, since readline iterates.
+        if inquery in current_line:
+            found = True
             break
-        if p == 5000000:
-            p = 0
-            pp += 1
-            print(5*pp,"m hashes checked.")
+        elif (inquery > current_line):
+            beginning = file.tell()
+        else:
+            ending = file.tell()
+    timed = (timeit.default_timer() - start_time)*1000
+
+if found:
+    print(f"Hashed version of password found. Number of times seen: " + current_line.split(":")[1])
+else:
+    print("Password not found in this file.")
+
+print(f"Time spent searching: {timed:.5} milliseconds")
